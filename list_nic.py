d353b1ef51aa5f62e5f3a4b7615679c7b9505d60
YJ-Work <yj1516268@outlook.com>
2020-12-04 17:00:59 +0800

    新增IPv6信息

--- list_nic.py
+++ list_nic.py
@@ -16,15 +16,24 @@ substitute = '/'
 
 ifaces = netifaces.interfaces()
 table = prettytable.PrettyTable()
-table.field_names = ['NIC', 'Address', 'Netmask', 'Broadcast']
+table.field_names = [
+    'NIC', 'Address (IPv4)', 'Netmask (IPv4)', 'Broadcast (IPv4)',
+    'Address (IPv6)', 'Netmask (IPv6)'
+]
 
 for iface in ifaces:
     iface_info = netifaces.ifaddresses(iface)
     table.add_row([
         iface,
-        iface_info.get(2, [dict()])[0].get('addr', substitute),
-        iface_info.get(2, [dict()])[0].get('netmask', substitute),
-        iface_info.get(2, [dict()])[0].get('broadcast', substitute)
+        iface_info.get(netifaces.AF_INET, [dict()])[0].get('addr', substitute),
+        iface_info.get(netifaces.AF_INET,
+                       [dict()])[0].get('netmask', substitute),
+        iface_info.get(netifaces.AF_INET,
+                       [dict()])[0].get('broadcast', substitute),
+        iface_info.get(netifaces.AF_INET6,
+                       [dict()])[0].get('addr', substitute),
+        iface_info.get(netifaces.AF_INET6,
+                       [dict()])[0].get('netmask', substitute),
     ])
 
 print(table)
